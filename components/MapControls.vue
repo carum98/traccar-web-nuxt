0abaec67bf2c0507acf141048d52346b5d5b005f
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-06-09 04:24:24 +0000

    Add map controls

---
+++ components/MapControls.vue
@@ -0,0 +1,127 @@
+<script setup lang="ts">
+const { zoom, setLayer } = useMap()
+
+const layer = ref(layers[0].value)
+
+watch(layer, (value) => {
+    const item = layers.find((item) => item.value === value)
+
+    if (item) {
+        setLayer(item.layer)
+    }
+})
+</script>
+
+<template>
+    <button class="layer-button" popovertarget="layers-dropdown">
+        <i class="icon-layer-group"></i>
+    </button>
+
+    <div id="layers-dropdown" popover>
+        <section>
+            <label v-for="item in layers">
+                {{ item.name }}
+                <input type="radio" :value="item.value" v-model="layer" />
+            </label>
+        </section>
+    </div>
+
+    <div class="zoom-buttons">
+        <button @click="zoom('in')">
+            <i class="icon-plus"></i>
+        </button>
+        <button @click="zoom('out')">
+            <i class="icon-minus"></i>
+        </button>
+    </div>
+</template>
+
+<style>
+.zoom-buttons {
+    background-color: var(--background-color);
+    border-radius: 10px;
+    display: flex;
+    flex-flow: column;
+    overflow: hidden;
+
+    position: relative;
+
+    button {
+        width: 35px;
+        height: 35px;
+        font-size: 16px;
+        transition: background-color 0.3s;
+
+        &:hover {
+            background-color: var(--item-background-color-hover);
+        }
+    }
+
+    &::before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: calc(50% - 10px);
+
+        width: 20px;
+        height: 1px;
+        background-color: var(--text-color);
+        opacity: 0.5;
+    }
+}
+
+.layer-button {
+    background-color: var(--background-color);
+    border-radius: 10px;
+    width: 35px;
+    height: 35px;
+    transition: background-color 0.3s;
+
+    anchor-name: --layers-dropdown;
+
+    &:hover {
+        background-color: var(--item-background-color-hover);
+    }
+}
+
+#layers-dropdown {
+    position-anchor: --layers-dropdown;
+    inset-area: left span-top;
+
+    padding: 10px;
+    border-radius: 10px;
+
+    section {
+        display: flex;
+        flex-flow: column;
+        gap: 5px;
+    }
+
+    label {
+        display: block;
+        cursor: pointer;
+
+        padding: 5px 15px;
+        border-radius: 10px;
+
+        transition: background-color 0.3s;
+
+        input[type="radio"] {
+            visibility: hidden;
+        }
+
+        &:has(input[type="radio"]:checked) {
+            background-color: var(--item-background-color-hover)
+        }
+
+        &:has(:not(input[type="radio"]:checked)) {
+            opacity: 0.7;
+        }
+
+        &:hover {
+            background-color: var(--item-background-color-hover)
+        }
+    }
+}
+</style>
--- composables/useMap.ts
+++ composables/useMap.ts
@@ -2,6 +2,59 @@ import L from 'leaflet'
 
 let map: L.Map
 
+export const layers = [
+    {
+        name: 'Stadia Dark',
+        value: 'stadia-dark',
+        layer: L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png')
+    },
+    {
+        name: 'OpenStreetMap',
+        value: 'openstreetmap',
+        layer: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    },
+    {
+        name: 'Google Maps',
+        value: 'google',
+        layer: L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}')
+    },
+    {
+        name: 'Google Satellite',
+        value: 'google-satellite',
+        layer: L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}')
+    },
+    {
+        name: 'Google Terrain',
+        value: 'google-terrain',
+        layer: L.tileLayer('https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}')
+    },
+    {
+        name: 'Google Hybrid',
+        value: 'google-hybrid',
+        layer: L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}')
+    },
+    {
+        name: 'Google Roads',
+        value: 'google-roads',
+        layer: L.tileLayer('https://mt1.google.com/vt/lyrs=t&x={x}&y={y}&z={z}')
+    },
+    {
+        name: 'Carto Dark',
+        value: 'carto-dark',
+        layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png')
+    },
+    {
+        name: 'Carto Voyager',
+        value: 'carto-voyager',
+        layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png')
+    },
+    {
+        name: 'Carto Positron',
+        value: 'carto-positron',
+        layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png')
+    },
+]
+
 export function useMap() {
     function init(id: string) {
         const element = document.getElementById(id) as HTMLElement
@@ -12,7 +65,7 @@ export function useMap() {
             zoomControl: false
         })
     
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+        setLayer(layers[0].layer)
     }
 
     function drawRoute(points: [number, number][]) {
@@ -45,15 +98,34 @@ export function useMap() {
         }
     }
 
+    function setLayer(layer: L.TileLayer) {
+        map.eachLayer((l) => {
+            if (l instanceof L.TileLayer) {
+                map.removeLayer(l)
+            }
+        })
+
+        layer.addTo(map)
+    }
+
     function flyTo(lat: number, lon: number) {
         map.flyTo([lat, lon], 18)
+    }
+
+    // Zoom
+    function zoom(type: 'in' | 'out') {
+        const currentZoom = map.getZoom()
+        const newZoom = type === 'in' ? currentZoom + 1 : currentZoom - 1
+        map.setZoom(newZoom)
     }
 
     return {
         init,
         drawRoute,
         setMarkers,
-        flyTo
+        flyTo,
+        zoom,
+        setLayer,
     }
 }
 
